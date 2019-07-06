4befc01e1d4bcf37504bd5d04ef425db8a9cd35e
Lucas <agtlucas@me.com>
2019-07-06 09:34:08 -0300

    Inline normalize.css and fonts

--- packages/components/src/Button/index.js
+++ packages/components/src/Button/index.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
-import { color, space, border, fontSize, buttonStyle, typography } from 'styled-system';
+import { color, space, border, layout, fontSize, buttonStyle, typography } from 'styled-system';
 import { font, buttonSizes } from "astro-core/src/custom-props";
 
 const Button = styled.button(
   {
-    appearance: 'button',
     cursor: 'pointer',
+    whiteSpace: 'no-wrap',
+    textAlign: 'center',
+    transition: 'all 0.3s ease',
+    overflow: 'hidden',
   },
   font,
   color,
   space,
   border,
+  layout,
   fontSize,
   typography,
   buttonStyle,
@@ -18,19 +22,21 @@ const Button = styled.button(
 );
 
 Button.defaultProps = {
+  display: 'inline-block',
   px: 4,
   py: 1,
-  font: 'primary',
-  size: 'medium',
   variant: 'primaryUranus',
+  size: 'medium',
+  font: 'primary',
 };
 
 Button.propTypes = {
   ...color.propTypes,
   ...space.propTypes,
+  ...border.propTypes,
+  ...layout.propTypes,
   ...fontSize.propTypes,
   ...buttonStyle.propTypes,
-  ...border.propTypes,
   ...typography.propTypes,
 };
 

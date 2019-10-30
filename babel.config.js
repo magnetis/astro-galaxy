038afb2852a2be043d9805f79bbcf5fa7e7acb9b
Renato Freire <contato.renatofreire@gmail.com>
2019-10-30 16:05:56 -0300

    Add tests flow
    - delete babel.rc from packages
    - add babel.config.js to packages
    - add babel.config.js in root
    - create test to components/button

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    'babel-plugin-styled-components',
+    '@babel/plugin-transform-react-jsx',
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true, useBuiltIns: true }],
+  ],
+  presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+  env: {
+    test: {
+      presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-react'],
+    },
+  },
+  ignore: ['node_modules/**'],
+};

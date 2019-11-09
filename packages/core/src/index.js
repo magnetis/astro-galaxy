9c9502d2bf7e1d96fbd7040d8988e41db4faa3a1
Lucas <agtlucas@me.com>
2019-11-09 10:41:18 -0300

    Remove dead code test utils

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,4 @@
 export { default as theme } from './themes/control';
 export { default as AstroThemeProvider } from './theme-provider';
 
-export * from './test-utils';
 export * from './custom-props';

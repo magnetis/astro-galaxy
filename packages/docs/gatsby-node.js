0c30e5ba015331f0755047f69b32e518dadc5c7e
Erica Prieto <erica.prieto@magnetis.com.br>
2019-12-11 15:46:37 -0300

    Fix astro-galaxy-icons alias on the docs resolver

--- packages/docs/gatsby-node.js
+++ packages/docs/gatsby-node.js
@@ -1,16 +1,19 @@
-const path = require("path")
+const path = require("path");
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
         "@magnetis/astro-galaxy-core": path.resolve(__dirname, "../core/src"),
-        "@magnetis/astro-galaxy-icons": path.resolve(__dirname, "../icons/src"),
+        "@magnetis/astro-galaxy-icons": path.resolve(
+          __dirname,
+          "../icons/lib/index.es"
+        ),
         "@magnetis/astro-galaxy-components": path.resolve(
           __dirname,
           "../components/src"
-        ),
-      },
-    },
-  })
-}
+        )
+      }
+    }
+  });
+};

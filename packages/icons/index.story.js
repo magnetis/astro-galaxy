4ca5a7fb4dd56f156a9e429689516910423ec1f3
Lucas <agtlucas@me.com>
2019-12-11 12:00:53 -0300

    Update icons accordingly to astro spec
    Co-Authored-By: Lari Maza <larimaza@users.noreply.github.com>

--- packages/icons/index.story.js
+++ packages/icons/index.story.js
@@ -3,9 +3,19 @@ import { storiesOf } from '@storybook/react';
 import map from 'lodash/map';
 import * as icons from './lib/index.es';
 
+/**
+  - To apply different sizes we shall use width and height props with the following values:
+  small = 24
+  medium = 32
+  large = 40
+  
+  - To apply spacing (margin-left and margin-right) we shall use ml (marginLeft) and mr (marginRight) with the following values:
+  
+  8px = 1 (space array index 1)
+**/
 const Story = () => {
   return map(icons, (Icon, IconName) => (
-    <Icon key={IconName} color="earth500" width="40" height="40" m={2} />
+    <Icon key={IconName} color="uranus500" width="40" height="40" m={1} />
   ));
 };
 

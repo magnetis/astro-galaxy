7085713cea9671e12b4139cfbbe2a01a39bd0d1d
Lucas <agtlucas@me.com>
2019-12-12 16:46:39 -0300

    Create buttons page

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,6 +1,9 @@
 export { default as Button } from './Button';
 export { default as Label } from './Label';
 export { default as OutlineButton } from './OutlineButton';
+export { default as IconButton } from './IconButton';
+export { default as IconGhostButton } from './IconGhostButton';
+export { default as IconOutlineButton } from './IconOutlineButton';
 
 export { default as Text } from './Text';
 export { default as Heading } from './Heading';

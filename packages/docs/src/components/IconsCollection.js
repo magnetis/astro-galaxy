3a6242c06a8da4b940ec8620526fe7ac89b851dd
Lucas <agtlucas@me.com>
2019-12-17 18:29:41 -0300

    Wip

--- packages/docs/src/components/IconsCollection.js
+++ packages/docs/src/components/IconsCollection.js
@@ -1,44 +1,150 @@
 import React from 'react';
 import styled from 'styled-components';
-import map from 'lodash/map';
 import * as icons from '@magnetis/astro-galaxy-icons';
 
 const Collection = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin-left: -16px;
+  margin: -12px -12px 25px;
 `;
 
 const Item = styled.div`
-  margin: 16px;
+  display: flex;
+  margin: 12px 12px 40px;
   text-align: center;
-  width: 72px;
+  flex-direction: column;
+  justify-content: flex-end;
 `;
 
 const Container = styled.div`
-  border: solid 1px silver;
+  border: solid 1px ${props => props.theme.colors.moon300};
   border-radius: 8px;
-  padding: 16px;
+  padding: 24px;
+  width: 82px;
+  height: 82px;
 `;
 
 const Name = styled.div`
   font-family: 'Poppins', Arial, Helvetica, sans-serif;
   font-size: 10px;
-  margin-top: 8px;
+  margin-top: 24px;
+  position: absolute;
+  align-self: center;
 `;
 
-const IconsCollection = () => (
+const dashboardIcons = [
+  'IconAdditional',
+  'IconChart',
+  'IconInvite',
+  'IconTime',
+  'IconDown',
+  'IconSheet',
+  'IconChat',
+  'IconFlag',
+  'IconTransactions',
+  'IconHeart',
+];
+
+const supportIcon = [
+  'IconAlert',
+  'IconArrowDown',
+  'IconArrowLeft',
+  'IconArrowRight',
+  'IconArrowUp',
+  'IconBalloon',
+  'IconBank',
+  'IconCake',
+  'IconCalendar',
+  'IconCalendarCheck',
+  'IconChartUp',
+  'IconChat',
+  'IconCircleCheck',
+  'IconCircleDown',
+  'IconCircleLeft',
+  'IconCircleLess',
+  'IconCircleMail',
+  'IconCircleMore',
+  'IconCircleRight',
+  'IconCircleUp',
+  'IconClock',
+  'IconClose',
+  'IconCoin',
+  'IconComparison',
+  'IconConfiguration',
+  'IconCopy',
+  'IconCup',
+  'IconDiagonalLeft',
+  'IconDiagonalRight',
+  'IconDocument',
+  'IconDollars',
+  'IconDown',
+  'IconDownload',
+  'IconDropDown',
+  'IconDropLeft',
+  'IconDropRight',
+  'IconDropUp',
+  'IconEyeClosed',
+  'IconEyeOpen',
+  'IconFloater',
+  'IconGift',
+  'IconHouse',
+  'IconInfo',
+  'IconKey',
+  'IconLaptop',
+  'IconLight',
+  'IconLocker',
+  'IconLogout',
+  'IconMagnifier',
+  'IconMail',
+  'IconMenu',
+  'IconMoneyBag',
+  'IconMountain',
+  'IconPencil',
+  'IconPhone',
+  'IconPhoto',
+  'IconPizza',
+  'IconPlay',
+  'IconProfile',
+  'IconReturn',
+  'IconSliders',
+  'IconTax',
+  'IconTrash',
+  'IconUpdate',
+  'IconUpload',
+  'IconWarning',
+  'IconXMen',
+];
+
+const DashboardIcons = () => (
   <Collection>
-    {icons &&
-      map(icons, (Icon, IconName) => (
+    {dashboardIcons.map(icon => {
+      const Tag = icons[icon];
+      return (
         <Item>
           <Container>
-            <Icon key={IconName} color="moon900" width="32" height="32" />
+            <Tag color="moon900" size={32} />
           </Container>
-          <Name>{IconName.replace('Icon', '')}</Name>
+          <Name>{icons[icon].name}</Name>
         </Item>
-      ))}
+      );
+    })}
   </Collection>
 );
 
-export default IconsCollection;
+const SupportIcons = () => (
+  <Collection>
+    {supportIcon.map(icon => {
+      const Tag = icons[icon];
+      return (
+        <Item>
+          <Container>
+            <Tag color="moon900" size={32} />
+          </Container>
+          <Name>{icons[icon].name}</Name>
+        </Item>
+      );
+    })}
+  </Collection>
+);
+
+export { DashboardIcons, SupportIcons };

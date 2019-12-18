4af9d79ebd5c92651a9612e1651964f971e87218
Lucas <agtlucas@me.com>
2019-12-18 08:57:34 -0300

    Improve filter for support icons
    Co-Authored-By: Lari Maza <larimaza@users.noreply.github.com>

--- packages/docs/src/components/IconsCollection.js
+++ packages/docs/src/components/IconsCollection.js
@@ -5,31 +5,25 @@ import * as icons from '@magnetis/astro-galaxy-icons';
 const Collection = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin: -12px -12px 25px;
+  margin-left: -16px;
 `;
 
 const Item = styled.div`
-  display: flex;
-  margin: 12px 12px 40px;
+  margin: 16px;
   text-align: center;
-  flex-direction: column;
-  justify-content: flex-end;
+  width: 72px;
 `;
 
 const Container = styled.div`
-  border: solid 1px ${props => props.theme.colors.moon300};
+  border: solid 1px silver;
   border-radius: 8px;
-  padding: 24px;
-  width: 82px;
-  height: 82px;
+  padding: 16px;
 `;
 
 const Name = styled.div`
   font-family: 'Poppins', Arial, Helvetica, sans-serif;
   font-size: 10px;
-  margin-top: 24px;
-  position: absolute;
-  align-self: center;
+  margin-top: 8px;
 `;
 
 const dashboardIcons = [
@@ -45,84 +39,16 @@ const dashboardIcons = [
   'IconHeart',
 ];
 
-const supportIcon = [
-  'IconAlert',
-  'IconArrowDown',
-  'IconArrowLeft',
-  'IconArrowRight',
-  'IconArrowUp',
-  'IconBalloon',
-  'IconBank',
-  'IconCake',
-  'IconCalendar',
-  'IconCalendarCheck',
-  'IconChartUp',
-  'IconChat',
-  'IconCircleCheck',
-  'IconCircleDown',
-  'IconCircleLeft',
-  'IconCircleLess',
-  'IconCircleMail',
-  'IconCircleMore',
-  'IconCircleRight',
-  'IconCircleUp',
-  'IconClock',
-  'IconClose',
-  'IconCoin',
-  'IconComparison',
-  'IconConfiguration',
-  'IconCopy',
-  'IconCup',
-  'IconDiagonalLeft',
-  'IconDiagonalRight',
-  'IconDocument',
-  'IconDollars',
-  'IconDown',
-  'IconDownload',
-  'IconDropDown',
-  'IconDropLeft',
-  'IconDropRight',
-  'IconDropUp',
-  'IconEyeClosed',
-  'IconEyeOpen',
-  'IconFloater',
-  'IconGift',
-  'IconHouse',
-  'IconInfo',
-  'IconKey',
-  'IconLaptop',
-  'IconLight',
-  'IconLocker',
-  'IconLogout',
-  'IconMagnifier',
-  'IconMail',
-  'IconMenu',
-  'IconMoneyBag',
-  'IconMountain',
-  'IconPencil',
-  'IconPhone',
-  'IconPhoto',
-  'IconPizza',
-  'IconPlay',
-  'IconProfile',
-  'IconReturn',
-  'IconSliders',
-  'IconTax',
-  'IconTrash',
-  'IconUpdate',
-  'IconUpload',
-  'IconWarning',
-  'IconXMen',
-];
+const excludedFromSupportIcons = [...dashboardIcons, 'IconInputDash', 'IconInputPlus'];
 
 const DashboardIcons = () => (
   <Collection>
     {dashboardIcons.map(icon => {
-      const Tag = icons[icon];
+      const Icon = icons[icon];
       return (
-        <Item>
+        <Item key={icon}>
           <Container>
-            <Tag color="moon900" size={32} />
+            <Icon color="moon900" size="32" />
           </Container>
           <Name>{icons[icon].name}</Name>
         </Item>
@@ -133,17 +59,19 @@ const DashboardIcons = () => (
 
 const SupportIcons = () => (
   <Collection>
-    {supportIcon.map(icon => {
-      const Tag = icons[icon];
-      return (
-        <Item>
-          <Container>
-            <Tag color="moon900" size={32} />
-          </Container>
-          <Name>{icons[icon].name}</Name>
-        </Item>
-      );
-    })}
+    {Object.keys(icons)
+      .filter(icon => !excludedFromSupportIcons.includes(icon))
+      .map(icon => {
+        const Icon = icons[icon];
+        return (
+          <Item key={icon}>
+            <Container>
+              <Icon color="moon900" size="32" />
+            </Container>
+            <Name>{icon}</Name>
+          </Item>
+        );
+      })}
   </Collection>
 );
 

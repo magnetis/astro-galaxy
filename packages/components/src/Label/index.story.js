a914efbc7a9ee0d650e5815df301bad0ecfca652
Lucas <agtlucas@me.com>
2020-04-23 10:40:18 -0300

    Remove old docs

--- packages/components/src/Label/index.story.js
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import Label from './index.js';
-
-storiesOf('typography|labels', module).add('default', () => <Label>Label</Label>);

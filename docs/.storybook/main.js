df379b5de0de8869d612baddb6d89b40e367dee8
El Marques <el@marques.ws>
2020-04-23 16:28:40 -0300

    Configure new docs deploy (#76)

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../docs/**/*.stories.(js|mdx)'],
+  stories: ['../src/**/*.stories.(js|mdx)'],
   addons: ['@storybook/addon-backgrounds', '@storybook/addon-docs', '@storybook/addon-knobs'],
 };
--- docs/.storybook/theme.js
+++ docs/.storybook/theme.js
@@ -1,5 +1,5 @@
 import { create } from '@storybook/theming/create';
-import { theme } from '../packages/core/src';
+import { theme } from '../../packages/core/src';
 
 export default create({
   base: 'light',

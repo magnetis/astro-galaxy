7f53f7700be3e3548e3a922ce0ce6ee823253ae3
Lucas <agtlucas@me.com>
2020-04-29 16:18:03 -0300

    Introduce @magnetis/astro-galaxy-themes

---
+++ packages/themes/src/mobile-app/index.d.ts
@@ -0,0 +1,40 @@
+import { Colors } from '@magnetis/astro-galaxy-tokens';
+
+export type Gradient = [string];
+
+export type GradientMap = {
+  sombrero: Gradient;
+  hoag: Gradient;
+  pinwheel: Gradient;
+  blackHole: Gradient;
+  milkyWay: Gradient;
+  whirlpool: Gradient;
+  nebulosa: Gradient;
+  andromeda: Gradient;
+  centaurus: Gradient;
+  cartwheel: Gradient;
+  mayall: Gradient;
+};
+
+export type Theme = {
+  colors: Colors;
+  gradients: GradientMap;
+  fonts: {
+    poppins: 'Poppins-Regular';
+    poppinsMedium: 'Poppins-Medium';
+    poppinsSemiBold: 'Poppins-SemiBold';
+    poppinsBold: 'Poppins-Bold';
+    poppinsBlack: 'Poppins-Black';
+    lato: 'Lato-Regular';
+    latoBold: 'Lato-Bold';
+  },
+  fontSizes: [12, 14, 16, 24, 32, 40, 48, 64];
+  radii: [0, 2, 8, 16, 24, 32];
+  space: [0, 8, 16, 24, 32, 40, 48, 64];
+  sizes: [0, '8px', '18px'];
+  zIndices: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+}
+
+declare const theme: Theme;
+
+export default theme;
--- packages/tokens/README.md
+++ packages/tokens/README.md
@@ -1,6 +1,6 @@
 # @magnetis/astro-galaxy-tokens
 
-> Astro Galaxy Components.
+> Astro Galaxy Tokens.
 
 See our repository [@magnetis/astro-galaxy](https://github.com/magnetis/astro-galaxy) for more information on how to use this package or the [issues](https://github.com/magnetis/astro-galaxy/issues?q=is%3Aopen+is%3Aissue+label%3Aastro-galaxy-tokens) associated with this package.
 
